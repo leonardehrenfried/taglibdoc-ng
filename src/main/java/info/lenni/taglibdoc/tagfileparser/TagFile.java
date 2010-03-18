d102fe96dd3dc7aac1003eb3f495c79f3a81f7bf
Lenni <leonard.ehrenfried@web.de>
2010-03-18 18:14:29 +0100

    Tagfile descriptions parsed & refactoring

--- src/main/java/info/lenni/taglibdoc/tagfileparser/TagFile.java
+++ src/main/java/info/lenni/taglibdoc/tagfileparser/TagFile.java
@@ -16,22 +16,19 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
- * @author Leonard Ehrenfried *
+ * @author Leonard Ehrenfried
  */
 public class TagFile {
 
     private static final String JSP_NS = "http://java.sun.com/JSP/Page";
+    // attributes to be read from the <jsp:directive.attribute/> tags
+    private static final String[] ATTRIBUTE_ATTRS = { "name", "description",
+            "required" };
+    // attributes to be read from the <jsp:directive.tag/> tags
+    private static final String[] TAG_ATTRS = { "name", "description",
+            "required" };
 
     private List<Directive> directives = new ArrayList<Directive>();
-    //private List<Attribute> attributes = new ArrayList<Attribute>();
-
-    public List<Directive> getDirectives() {
-        return directives;
-    }
-
-    public void addDirective(Directive directive) {
-        directives.add(directive);
-    }
 
     public static TagFile parse(InputStream input) {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -43,36 +40,71 @@ public class TagFile {
 
             TagFile tagfile = new TagFile();
 
-            NodeList directiveElms = doc.getElementsByTagNameNS(JSP_NS,
+            NodeList attrNodes = doc.getElementsByTagNameNS(JSP_NS,
                     "directive.attribute");
+            List<Directive> directives = convertToDirectives(attrNodes,
+                    "attribute", ATTRIBUTE_ATTRS);
+            tagfile.addDirectives(directives);
 
-            for (int i = 0; i < directiveElms.getLength(); i++) {
+            NodeList tagNodes = doc.getElementsByTagNameNS(JSP_NS,
+                    "directive.tag");
+            List<Directive> tags = convertToDirectives(tagNodes, "tag",
+                    TAG_ATTRS);
+            tagfile.addDirectives(tags);
 
-                Node currentAttribute = directiveElms.item(i);
-                NamedNodeMap attrs = currentAttribute.getAttributes();
-
-                String name = attrs.getNamedItem("name").getNodeValue();
-                Attribute attribute = new Attribute("name", name);
-
-                Directive directive=new Directive();
-                directive.setDirectiveName("attribute");
-                directive.addAttribute(attribute);
-                tagfile.addDirective(directive);
-                
-            }
-
+            System.out.println(tagfile);
             return tagfile;
 
         } catch (ParserConfigurationException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (SAXException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         return null;
     }
+
+    private static List<Directive> convertToDirectives(NodeList nodeList,
+            String directiveName, String[] allowedAttrs) {
+
+        List<Directive> directives = new ArrayList<Directive>();
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+
+            Node currentAttribute = nodeList.item(i);
+            NamedNodeMap attrs = currentAttribute.getAttributes();
+
+            Directive directive = new Directive();
+            directive.setDirectiveName(directiveName);
+
+            for (String name : allowedAttrs) {
+
+                Node node = attrs.getNamedItem(name);
+                if (node != null) {
+                    String value = node.getNodeValue();
+                    Attribute attribute = new Attribute(name, value);
+                    directive.addAttribute(attribute);
+                }
+            }
+
+            directives.add(directive);
+        }
+        return directives;
+    }
+
+    // getters & setters
+
+    public List<Directive> getDirectives() {
+        return directives;
+    }
+
+    public void addDirective(Directive directive) {
+        directives.add(directive);
+    }
+
+    public void addDirectives(List<Directive> directives) {
+        this.directives.addAll(directives);
+    }
+
 }
